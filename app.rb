0a9b1b4f7e40ee85087bb07af37587af39d51364
Edward Ward <edwardwardward@gmail.com>
2017-08-02 18:06:04 +0100

    add set up

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require './lib/event'
 require './lib/events'
+require './lib/ticket'
 
-@events = Events.new
-@events.random_events(Event)
+def run_app
+  set_up
+  user_choice
+end
+
+def user_choice
+  puts "Please input Coordinates"
+end
+
+def set_up
+  events = Events.new
+  events.random_events(Event)
+  events.event_list.each {|event| 5.times do
+    event.add_ticket(Ticket)
+    end
+  }
+end
+
+run_app
